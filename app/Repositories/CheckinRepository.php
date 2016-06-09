0e863a04c12bc8770625155719f397dac1dc9f00
Robert Vrabel <rvrabel@gmail.com>
2016-06-09 19:14:48 -0400

    Make sure we are injecting interfaces

--- app/Repositories/CheckinRepository.php
+++ app/Repositories/CheckinRepository.php
@@ -16,14 +16,16 @@ class CheckinRepository implements CheckinRepositoryContract
     }
 
     /**
-     * Find the users checkin
+     * Find a record by a column
      *
-     * @param $username
-     * @return mixed
+     * @param $attribute
+     * @param $value
+     * @param array $columns
+     * @return
      */
-    public function find($username)
+    public function findBy($attribute, $value, array $columns = ['*'])
     {
-        return $this->checkin->where('username', $username)->first();
+        return $this->checkin->where($attribute, $value)->first($columns);
     }
 
     /**
